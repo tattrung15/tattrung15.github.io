fb2ab36766e03b268f5de5e81e5bc7613a19bbc1
Bùi Tất Trung <tattrung15@users.noreply.github.com>
2020-03-24 18:36:15 +0700

    a

--- LearnJS/BT3/script.js
+++ LearnJS/BT3/script.js
@@ -16,11 +16,59 @@ function showInfo(){
     info.innerHTML = JSON.stringify(issue, null, 4);
 }
 
+function closeForm(){
+    let bgForm = document.getElementsByClassName('view-form')[0];
+    bgForm.classList.remove('d-block');
+}
+
 function showForm(){
     let bgForm = document.getElementsByClassName('view-form')[0];
     let transparent = document.getElementsByClassName('transparent')[0];
+
     bgForm.classList.add('d-block');
+
     transparent.addEventListener('click', () => {
         bgForm.classList.remove('d-block');
     });
+
+    document.frm.title.value = issue.title;
+    document.frm.description.value = issue.description;
+    document.frm.type.value = issue.type;
+    document.frm.priority.value = issue.priority;
+    document.frm.createdBy.value = issue.createdBy;
+    document.frm.assignee.value = issue.assignee;
+    document.frm.estimate.value = issue.estimate;
+    document.frm.state.value = issue.state;
+    for(let i = 0; i < issue.allowFor.length; i++){
+        document.getElementById(issue.allowFor[i]).checked = true;
+    }
+
+}
+
+function saveChanges(){
+    let info = document.getElementById('info');
+
+    issue.title = document.frm.title.value;
+    issue.description = document.frm.description.value;
+    issue.type = document.frm.type.value;
+    issue.priority = document.frm.priority.value;
+    issue.allowFor = getAllowFor();
+    issue.createdBy = document.frm.createdBy.value;
+    issue.assignee = document.frm.assignee.value;
+    issue.estimate = document.frm.estimate.value;
+    issue.state = document.frm.state.value;
+    info.innerHTML = JSON.stringify(issue, null, 4);
+
+    closeForm();
+}
+
+function getAllowFor(){
+    let allowFor = [];
+
+    document.frm.allowFor.forEach(item => {
+        if(item.checked){
+            allowFor.push(item.value);
+        }
+    });
+    return allowFor;
 }
